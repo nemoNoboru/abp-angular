b7a029273c8e12eebb65e6eddfa068a4a0bd1f7e
nemoNoboru <felipetavres@gmail.com>
2017-01-06 14:33:04 +0100

    tablas done. falta hacer modales de success

--- app/tablas/tablasComponent.js
+++ app/tablas/tablasComponent.js
@@ -1,9 +1,16 @@
-app.controller('tablaCtrl',['$scope','$firebaseArray',function($scope,$firebaseArray) {
+app.controller('tablaCtrl',['$scope','$firebaseArray','$firebaseObject',function($scope,$firebaseArray,$firebaseObject) {
   var ref = firebase.database().ref().child('Exercises');
   $scope.ejercicios = $firebaseArray(ref);
 
+  var self = this;
+
   ref = firebase.database().ref().child('Tables');
   $scope.tablas = $firebaseArray(ref);
+
+  // borramos una tabla dada la ID pasada
+  $scope.deleteTable = function(id) {
+    $firebaseObject(firebase.database().ref().child('Tables').child(id)).$remove();
+  }
 
   // agregamos al array llamado ejercicios dentro de una estancia seleccionada de tables
   // el id del ejercicio a agregar
@@ -11,9 +18,33 @@ app.controller('tablaCtrl',['$scope','$firebaseArray',function($scope,$firebaseA
     $scope.activeejercicios.$add(ejercicio);
   };
 
+  self.recharge = function() {
+    $scope.showejericios = [];
+    $scope.activeejercicios.forEach(function(e) {
+      console.log(e.$value);
+      $scope.showejericios.push($firebaseObject(firebase.database().ref().child('Exercises').child(e.$value)));
+    });
+  };
   // recargamos los ejercicios pertenecientes a la tabla
   $scope.reload = function(activeid) {
+    $scope.showejericios = [];
     var ref = firebase.database().ref().child('Tables').child(activeid).child('ejercicios');
     $scope.activeejercicios = $firebaseArray(ref);
+    $scope.activeejercicios.$watch(function(e) {
+      console.log(e);
+      self.recharge()
+      console.log($scope.showejericios);
+    });
   };
-}])
+
+  // borramos la id del ejercicio de la tabla activa si nos lo pide el usuario
+  $scope.delete = function(record) {
+    // vamos a buscar el indice del record
+    var indice = $scope.showejericios.indexOf(record);
+
+    // sabiendo el indice borramos la id de ese indice
+    $scope.activeejercicios.$remove(indice);
+
+  };
+
+}]);
